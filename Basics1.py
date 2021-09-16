61f21b624481fac66d57256f4e61de4f41821fc3
Gullian <gullianvdwalt1@gmail.com>
2021-09-16 07:50:44 +0200

    init

---
+++ Basics1.py
@@ -0,0 +1,39 @@
+import datetime
+
+# Print
+#print(datetime.datetime.now())
+
+
+# Variables
+#dateTime = datetime.datetime.now()
+#num1, num2 = 10,20
+#num3 = num1 + num2
+#print(num3, dateTime)
+
+#vString = "John"
+#vInt = 100
+#vFloat = 10.22
+
+#print("String: ", vString, " Integer: ", vInt, " Float: ", vFloat)
+
+# Lists
+#studenGrades = [40, 40.2, 50, 50.44, 60]
+#for grade in studenGrades:
+#print(grade)
+#print("Average: ", ((sum(studenGrades)) / len(studenGrades)))
+
+#Ranges
+#print(list(range(15, 200, 3)))
+
+# dir
+# Check wat can be done with a list
+#print(dir(studenGrades))
+
+# Dictionary Types (Object)
+#student_grades = {"Marry": 40, "John": 50}
+#print(student_grades.values())
+#print(student_grades.keys())
+
+# Tuple Types (final)
+#temps = (1, 4, 5)
+
---
+++ Basics2.py
@@ -0,0 +1,42 @@
+temps = [10, 20, 30.2, 19, 15, 14]
+
+# Append
+#temps.append(20)
+#print(temps)
+
+# Index
+#print(temps.index(20))
+
+# Clear
+#temps.clear()
+#print(temps)
+
+#temps.__getitem__(1)
+#print(temps[1])
+
+# Slicing
+#firstThree = temps[:5]
+#print(firstThree)
+
+# Get last two
+#print(temps[-2::])
+
+#msg = 'hello'
+#print(msg[0])
+#print(msg[-1])
+#print(msg[1::])
+
+# Access Dictionary
+#student_grades = {"Marry": 40, "John": 50, "Peter": 10}
+#print(student_grades["Marry"])
+
+# Converting Lists
+
+# List to tuple
+#myList = [10, 20, 30]
+#myTuple = tuple(myList)
+print(myTuple)
+# Tuple to List
+#myTuple = (10, 20, 30)
+#myList = list(myTuple)
+#print(myList)
