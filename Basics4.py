186654669d1a8b95d155d7a6f08b3e68f05d786f
Gullian <gullianvdwalt1@gmail.com>
2021-09-26 07:42:31 +0200

    Loops

---
+++ Basics4.py
@@ -0,0 +1,13 @@
+temperatures = [0.1, 9.2, 8, 10, 12, 15, 22, 33, 21]
+
+for temp in temperatures:
+    print("Temp: ", temp)
+
+studentGrades = {"John": 20, "Marry": 10, "Peter": 14}
+for grade in studentGrades.items():
+    print(grade)
+for grade in studentGrades.keys():
+    print(grade)
+for grade in studentGrades.values():
+    print(grade)
+
